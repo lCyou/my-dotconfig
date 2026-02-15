7d87212280e8d5b906919e37a8268bf3ae2ea6d2
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2026-02-15 18:37:07 +0900

    add: telescope.nvim

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,14 +1,31 @@
+local options = {
+	theme = "auto",
+	component_separators = {
+		right = "",
+		left = "",
+	},
+	section_separators = {
+		rigth = "",
+		left = "",
+	},
+	globalstatus = true,
+}
+
+local sections = {}
+
+local inactive_sections = {}
+
+local tabline = {}
+
+local extensions = {}
+
 return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" }, 
     config = function()
       require("lualine").setup({
-        options = {
-          theme = "tokyonight",
-          component_separators = "|",
-          section_separators = "",
-        },
+        options = options,
       })
     end,
   },
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,36 @@
+local telescope_opts = {
+    defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+            },
+        },
+        path_display = { "smart" },
+    },
+}
+
+local M = {
+    "nvim-telescope/telescope.nvim",
+    version = '*',
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+}
+
+M.config = function()
+    local telescope = require("telescope")
+    telescope.setup(telescope_opts)
+    telescope.load_extension("fzf")
+    
+    -- キーマップ設定
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+end
+
+return M
