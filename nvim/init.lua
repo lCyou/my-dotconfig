662f5afc6c9975849f4078778db79b7bdbd95fcc
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2026-02-17 00:02:12 +0900

    chore: update looks setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
+-- leader key
+vim.g.mapleader = " "
+
 require('core')
 require('keymap')
 require('plugin')
@@ -5,10 +8,14 @@ require("config.lazy")
 
 vim.opt.number = true         -- 行番号を表示 (nu)
 vim.opt.relativenumber = true  -- 相対行番号を表示 (rnu)
+vim.opt.signcolumn = "yes"     -- サインカラムを常時表示（Git差分記号用、幅固定でガタつき防止）
 vim.opt.cursorline = true -- cursor行をハイライト
 vim.opt.cursorcolumn = true -- cursor列をハイライト
-vim.opt.termguicolors = ture -- true color 対応
+vim.opt.termguicolors = true -- true color 対応
 vim.opt.smartindent = true -- indent設定
+
+-- lualineとの重複のため削除
+vim.opt.showmode = false
 
 vim.api.nvim_create_user_command(
     'InitLua',
