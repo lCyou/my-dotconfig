fcf2c1241cfdcf40ea384b3ca5e7ec9639cfae0f
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2026-03-02 23:27:28 +0900

    add which-key

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,62 @@
+-- which-key.nvim: キーマップをポップアップで表示するヘルパー
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 200  -- 200ms後にポップアップ表示
+  end,
+  config = function()
+    local wk = require("which-key")
+    
+    wk.setup({
+      -- シンプルな表示スタイル
+      preset = "modern",
+      delay = 200,
+      
+      -- ウィンドウ設定
+      win = {
+        border = "rounded",
+        padding = { 1, 2 },
+      },
+      
+      -- レイアウト設定
+      layout = {
+        spacing = 3,
+        align = "left",
+      },
+      
+      -- アイコン設定
+      icons = {
+        breadcrumb = "»",
+        separator = "➜",
+        group = "+",
+      },
+      
+      -- ビルトインマッピングを表示
+      spec = {
+        -- 基本的なVimコマンド
+        { "g", group = "goto" },
+        { "gt", desc = "Go to next tab" },
+        { "gT", desc = "Go to previous tab" },
+        { "z", group = "fold" },
+        { "]", group = "next" },
+        { "[", group = "prev" },
+        { "<leader>", group = "leader" },
+        
+        -- Leaderキーのグループ定義
+        { "<leader>f", group = "Find (Telescope)" },
+        { "<leader>g", group = "Git" },
+        { "<leader>l", group = "LSP" },
+        { "<leader>ln", desc = "LSP: Rename" },
+        { "<leader>la", desc = "LSP: Code Action" },
+        { "<leader>lf", desc = "LSP: Format (Prettier)" },
+        { "<leader>lo", desc = "LSP: Organize Imports" },
+        { "<leader>ll", desc = "LSP: Lint (ESLint)" },
+        { "<leader>e", desc = "Explorer (NeoTree)" },
+        { "<leader>t", desc = "Tab: New" },
+        { "<leader>w", desc = "Tab: Close" },
+      },
+    })
+  end,
+}
